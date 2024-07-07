13bb92ccd7eca5985d3b9b7717cbb54a0033fe57
Cal <cwolfson58@gmail.com>
2024-07-08 01:19:06 +1000

    add caching, convert to object

--- src/main/kotlin/com/calmwolfs/valorantmodelapi/ValorantModelApi.kt
+++ src/main/kotlin/com/calmwolfs/valorantmodelapi/ValorantModelApi.kt
@@ -20,51 +20,59 @@ import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URL
 
-class ValorantModelApi {
+object ValorantModelApi {
 
     private val projectVersion = System.getProperty("project.version")
-    private val baseUrl = "https://valorant-api.com/v1"
+    private const val BASE_URL = "https://valorant-api.com/v1"
 
-    fun getAgents() = sendRequestList<Agent>("agents")
-    fun getBuddies() = sendRequestList<Buddy>("buddies")
-    fun getCards() = sendRequestList<Card>("playercards")
-    fun getContentTiers() = sendRequestList<ContentTier>("contenttiers")
-    fun getCurrencies() = sendRequestList<Currency>("currencies")
-    fun getGamemodes() = sendRequestList<Gamemode>("gamemodes")
-    fun getGamemodeEquippable() = sendRequestList<GamemodeEquippable>("gamemodes/equippables")
-    fun getMaps() = sendRequestList<ValorantMap>("maps")
-    fun getSeasons() = sendRequestList<Season>("seasons")
-    fun getThemes() = sendRequestList<Theme>("themes")
-    fun getTitles() = sendRequestList<Title>("playertitles")
-    fun getWeapons() = sendRequestList<Weapon>("weapons")
-    fun getVersion() = sendRequest<Version>("version")
-    fun getSprays() = sendRequestList<Spray>("sprays")
+    private val requestCache = mutableMapOf<String, Any>()
+
+    fun getAgents(force: Boolean = false) = sendRequestList<Agent>("agents", force)
+    fun getBuddies(force: Boolean = false) = sendRequestList<Buddy>("buddies", force)
+    fun getCards(force: Boolean = false) = sendRequestList<Card>("playercards", force)
+    fun getContentTiers(force: Boolean = false) = sendRequestList<ContentTier>("contenttiers", force)
+    fun getCurrencies(force: Boolean = false) = sendRequestList<Currency>("currencies", force)
+    fun getGamemodes(force: Boolean = false) = sendRequestList<Gamemode>("gamemodes", force)
+    fun getGamemodeEquippable(force: Boolean = false) = sendRequestList<GamemodeEquippable>("gamemodes/equippables", force)
+    fun getMaps(force: Boolean = false) = sendRequestList<ValorantMap>("maps", force)
+    fun getSeasons(force: Boolean = false) = sendRequestList<Season>("seasons", force)
+    fun getThemes(force: Boolean = false) = sendRequestList<Theme>("themes", force)
+    fun getTitles(force: Boolean = false) = sendRequestList<Title>("playertitles", force)
+    fun getWeapons(force: Boolean = false) = sendRequestList<Weapon>("weapons", force)
+    fun getVersion(force: Boolean = false) = sendRequest<Version>("version", force)
+    fun getSprays(force: Boolean = false) = sendRequestList<Spray>("sprays", force)
 
     @Throws(IOException::class)
-    private inline fun <reified T> sendRequest(requestPath: String): T {
+    private inline fun <reified T : Any> sendRequest(requestPath: String, force: Boolean): T {
+        if (!force && requestCache.containsKey(requestPath)) {
+            return requestCache[requestPath] as T
+        }
         val responseJsonObject = getRawJsonResponse(requestPath)
-        return GsonUtils.gson.fromJson(responseJsonObject.getAsJsonObject("data"), T::class.java)
+        val result = GsonUtils.gson.fromJson(responseJsonObject.getAsJsonObject("data"), T::class.java)
+        requestCache[requestPath] = result
+        return result
     }
 
     @Throws(IOException::class)
-    private inline fun <reified T> sendRequestList(
-        requestPath: String,
-    ): List<T> {
+    @Suppress("UNCHECKED_CAST")
+    private inline fun <reified T : Any> sendRequestList(requestPath: String, force: Boolean, ): List<T> {
+        if (!force && requestCache.containsKey(requestPath)) {
+            return requestCache[requestPath] as List<T>
+        }
         val responseJsonObject = getRawJsonResponse(requestPath)
         val data = responseJsonObject.getAsJsonArray("data")
         val result = mutableListOf<T>()
         data.forEach {
             result.add(GsonUtils.gson.fromJson(it, T::class.java))
         }
+        requestCache[requestPath] = result
         return result
     }
 
     @Throws(IOException::class)
     private fun getRawJsonResponse(requestPath: String): JsonObject {
-        val url = "$baseUrl/$requestPath"
-
+        val url = "$BASE_URL/$requestPath"
         val connection = URL(url).openConnection() as HttpURLConnection
-        println("connecting to $url")
 
         connection.requestMethod = "GET"
         connection.setRequestProperty("User-Agent", "ValorantModelApi/$projectVersion")
